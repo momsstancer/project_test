1aadc6bffa19d32d210b1cc29f7b1943e55288fd
stratan.p <stratan.p@soft2bet.com>
2022-11-14 13:29:40 +0200

    lesson_2_changes

--- lesson_2/int.py
+++ lesson_2/int.py
@@ -1,8 +1,8 @@
 int_var = 12332111223486981642391332135
-int_var1 = str(int_var)
-int_var2 = list(int_var1)
-int_var3 = set(int_var2)
-int_var4 = sorted(int_var3)
-int_var4.sort(reverse=True)
+str_var = str(int_var)
+list_var = list(str_var)
+set_var = set(list_var)
+sort_var = sorted(set_var)
+sort_var.sort(reverse=True)
 
-print(int_var4)
+print(sort_var)
