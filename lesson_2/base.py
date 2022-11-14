52cbfde4cb1294e853236c11f2d72779b02eb97a
stratan.p <stratan.p@soft2bet.com>
2022-11-14 12:42:26 +0200

    1_lesson

---
+++ lesson_2/base.py
@@ -0,0 +1,31 @@
+base = {
+    "login": "Test_login",
+    "password": "Yurec1989",
+    "retype": "Yurec1989",
+    "accept": True,
+    "avatar": "banana",
+    "address": "asqdrrrr",
+    "birthday": "1989-01-10",
+    "city": "sdaddqddd",
+    "country": "FI",
+    "currency": "EUR",
+    "email": "WERTYrU@ASDFffqaffrfff",
+    "gdpr_data": True,
+    "gdpr_marketing": True,
+    "gender": "M",
+    "name": "sdfghffaffrff",
+    "phone": 380529876025,
+    "postcode": 12345643245,
+    "product": "test",
+    "surname": "uoieccccccccac",
+    "trigger_codes": "casino-welcome-bonus",
+    "qa": {},
+    "birth_country": "DE",
+    "nationality": "DE",
+    "birth_name": "dasdasda",
+    "birth_place": "Bavaria",
+    "loss_limit": {"amount": 1000, "period": "week"},
+}
+
+print(base.values())
+print(base.keys())
